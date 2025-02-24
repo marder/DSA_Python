7bd068f35678dc361ccfb994f17e52aa156b9e3d
marder <marcin.derucki@gmail.com>
2025-02-24 17:46:48 +0100

    24.02.2025

---
+++ 01_bubble_sort_iter.py
@@ -0,0 +1,21 @@
+def bubble_sort(lst):
+
+    size = len(lst)
+
+    for i in range(size):
+
+        for j in range(size - 1):
+
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+
+    return lst
+
+
+unsorted_list = [69, 6, 15, 0, 55, 4]
+
+print(f"Unsorted List: {unsorted_list}")
+
+sorted_list = bubble_sort(unsorted_list)
+
+print(f"Sorted List: {sorted_list}")
